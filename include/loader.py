42c726303c374bb9108bc5aed468e62bbea7adbf
Brandon Musgrave <musgr017@umn.edu>
2019-03-27 20:28:08 -0500

    Added setServoAngle

--- include/loader.py
+++ include/loader.py
@@ -5,6 +5,24 @@
 ########################################################
 
 import os
+import RPi.GPIO as GPIO
+from time import sleep
+def setServoAngle(pin, deg):
+	if deg>60:
+		return False
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(pin, GPIO.OUT)
+	pwm=GPIO.PWM(pin, 50)
+	pwm.start(0)
+	duty= deg/18 + 2
+	GPIO.output(pin, True)
+	pwm.ChangeDutyCycle(duty)
+	sleep(1)
+	GPIO.output(pin, False)
+	pwm.ChangeDutyCycle(0)
+	pwm.stop()
+	GPIO.cleanup()
+	return True
 
 class Loader:
 
