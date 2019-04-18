4022733c817f3cd037252981900cb34833c07a70
Nick Knudsen <nickknudsen3@gmail.com>
2019-04-17 19:28:11 -0500

    Update engine to run sample servo program

--- engine.py
+++ engine.py
@@ -6,12 +6,27 @@
 
 import os
 from include.listener import Listener
+from include.loader import Loader
+import time
 
 def engine():
     print("Running engine.py")
 
-    myListener = Listener()
-    myListener.printName()
+    myLoader = Loader()
+
+    myLoader.setServoAngle(18,10)
+    time.sleep(5)
+    myLoader.setServoAngle(13,29)
+    time.sleep(5)
+    myLoader.setServoAngle(18, 30)
+    myLoader.setServoAngle(13, 10)
+
+    print("Finished running engine.py")
+
+    #mqttc = Listener()
+    #rc = mqttc.run()
+
+    #print("rc: " + str(rc))
 
 
 if __name__ == "__main__":
